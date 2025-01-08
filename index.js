4d5158003567005f7d81ea5a4ea2adef422681a7
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-08 17:51:13 +0500

    storage token in cli weather

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import {printError,printSuccess,printHelp} from "./services/log.service.js"
 import getArgs from  './helpers/args.js'
+import { saveKeyValue } from "./services/storage.service.js"
+const saveToken=async token=>{
+    try{
+        await saveKeyValue('token',token);
+        printSuccess("token was saved")
+    }catch (error){
+        printError(error.message)
+    }
+    
 
+}
 
 const startcli=()=>{
     const args=getArgs(process.argv)
+   // console.log(args)
     // printSuccess("GOOD/OK")
     // printError("no ok")
    // console.log(args)
@@ -16,6 +27,7 @@ const startcli=()=>{
     }
     if(args.t){
         //save token
+        return saveToken(args.t);
     }
     //resault
 }
